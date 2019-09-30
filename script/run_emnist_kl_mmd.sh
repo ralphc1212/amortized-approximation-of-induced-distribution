337918fb02e6ba8b1e46baa685d19ad34852568a
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2019-09-30 23:29:21 +0800

    Add files via upload

--- script/run_emnist_kl_mmd.sh
+++ script/run_emnist_kl_mmd.sh
@@ -2,3 +2,4 @@ python ../src/train_teacher_mcdp_emnist.py --from-model 0
 
 python ../src/amt_approx_emnist_KL.py --from-approx-model 0 --test-ood-from-disk 0
 
+python ../src/amt_approx_emnist_mmd.py --from-approx-model 0 --test-ood-from-disk 0
