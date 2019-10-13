fc4d40a9fada34d09d63420cb932a3942d18d360
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2019-10-13 21:29:23 +0800

    Delete run_emnist_kl_mmd.sh

--- script/run_emnist_kl_mmd.sh
+++
@@ -1,5 +0,0 @@
-python ../src/train_teacher_mcdp_emnist.py --from-model 0
-
-python ../src/amt_approx_emnist_KL.py --from-approx-model 0 --test-ood-from-disk 0
-
-python ../src/amt_approx_emnist_mmd.py --from-approx-model 0 --test-ood-from-disk 0
