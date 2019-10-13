31b4f255008d6b2dba7bca645ca3032692d4f686
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2019-10-13 21:29:45 +0800

    Delete run_mnist_sgld_kl_mmd.sh

--- script/run_mnist_sgld_kl_mmd.sh
+++
@@ -1,5 +0,0 @@
-python ../src/train_teacher_sgld_mnist.py --from-model 0
-
-python ../src/amt_approx_mnist_sgld_KL.py --from-approx-model 0 --test-ood-from-disk 1
-
-python ../src/amt_approx_mnist_sgld_mmd.py --from-approx-model 0 --test-ood-from-disk 1
